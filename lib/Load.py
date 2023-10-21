48016249a50d7509b06e692f05473a1aba95d2f7
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-10-21 15:05:44 -0400

    Create Load.py

---
+++ lib/Load.py
@@ -0,0 +1,34 @@
+import csv
+import sqlite3
+
+def load(dataset="SPX.csv"):
+    # Connect to SQLite database
+    conn = sqlite3.connect('Index.db')
+    cursor = conn.cursor()
+
+    # Create a table named 'SPX'
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS SPX (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DOUBLE,
+        open DOUBLE,
+        high DOUBLE,
+        low DOUBLE,
+        close DOUBLE,
+        adj_close DOUBLE,
+        volume DOUBLE,
+    )
+    ''')
+
+    # Read the CSV file and insert data into the SQLite database
+    with open(dataset, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            cursor.execute('''
+            INSERT INTO SPX (date, open, high, low, close, adj_close, volume) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (row[Date], row[Open], row[High], row[Low], row[Close], row[Adj Close], row[Volume]))
+
+    # Commit changes and close the SQLite connection
+    conn.commit()
+    conn.close()
